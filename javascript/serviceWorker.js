9ab2f6de0701469af4a0de9793c1a357e4b80334
Erik <Erikpersson0884@gmail.com>
2022-07-11 22:02:19 +0200

    added the service worker

--- javascript/serviceWorker.js
+++ javascript/serviceWorker.js
@@ -1,26 +1,76 @@
-var cacheName = 'hello-pwa';
-var filesToCache = [
-  '/index.html',
-  '/css/*',
-  '/js/*',
-  '/img/*.png'
-];
 
-/* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
 });
 
-/* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+
+// cache files
+
+const cacheName = "epw_PWA-v1";
+const appShellFiles = [
+  '/',
+  '/index.html',
+  '/css',
+  '/css/style.css',
+  '/css/fonts/',
+  '/javascript/',
+  '/javascript/script.js'
+]
+
+
+
+const images = [];
+for (let i = 0; i < pic.length; i++) {
+  images.push(`img/${pic[i].slug}.png`);
+}
+
+
+// Combine arrays
+
+const contentToCache = appShellFiles.concat(images);
+
+
+
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil((async () => {
+    const cache = await caches.open(cacheName);
+    console.log('[Service Worker] Caching all: app shell and content');
+    await cache.addAll(contentToCache);
+  })());
+});
+
+
+
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith((async () => {
+    const r = await caches.match(e.request);
+    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+    if (r) { return r; }
+    const response = await fetch(e.request);
+    const cache = await caches.open(cacheName);
+    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+    cache.put(e.request, response.clone());
+    return response;
+  })());
+});
+
+
+
+
+
+
+
+
+// Clear old cache
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((keyList) => {
+    return Promise.all(keyList.map((key) => {
+      if (key === cacheName) { return; }
+      return caches.delete(key);
+    }))
+  }));
 });
 
